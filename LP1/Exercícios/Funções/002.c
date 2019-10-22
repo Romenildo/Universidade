3ba682a3d3b1713f78c71247a5153f5a4bbf2ebf
♛    ∻Rom∻    ♚ <53869976+Romenildo@users.noreply.github.com>
2019-10-22 07:52:27 -0300

    Exercicio 002 da lista :skull:
    Escreva uma função que receba um número e calcule o seu dobro.

---
+++ LP1/Exercícios/Funções/002.c
@@ -0,0 +1,18 @@
+/*2. Escreva uma função que receba um número e calcule o seu dobro.*/
+
+#include <stdio.h>
+
+int dobro(int num){
+
+    return num*2;
+}
+
+
+main(){
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d",&num);
+
+    printf("o dobro de %d eh %d",num,dobro(num));
+
+}
