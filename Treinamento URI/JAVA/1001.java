7965e317ca3de57ffdb23d41b63f9ff2c8295b46
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2020-06-08 15:44:12 -0300

    Create 1001.java

---
+++ Treinamento URI/JAVA/1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+      //Decaracoes de variaveis e o Scanner
+      Scanner scan = new Scanner(System.in);
+      int A,B,X;
+      //Codigo
+      A = scan.nextInt();
+      B = scan.nextInt();
+      X = A + B;
+
+      System.out.println("X = "+ X);
+
+
+    }
+}
