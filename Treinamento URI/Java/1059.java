33d7c32ccc48d46788aa3a8cc5ca60ceaa645d73
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2020-06-11 08:58:11 -0300

    Create 1059.java

---
+++ Treinamento URI/Java/1059.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		for(int i=1 ;i<=100;i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
