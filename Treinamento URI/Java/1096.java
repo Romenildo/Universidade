5e5b530f61701808b35227fcd3af0f4c7a3b5442
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2020-06-11 14:27:08 -0300

    Update and rename 1096f.java to 1096.java

---
+++ Treinamento URI/Java/1096.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[] args) {
+		
+		for(int i=1 ; i<10 ;i=i+2) {
+			for(int j=7; j >= 5 ; j--) {
+				System.out.printf("I=%d J=%d\n",i,j);
+			}
+		}
+	}
+}
