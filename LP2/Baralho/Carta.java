091710a40fc06553a764727ff7ae2dce0fbf793c
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2020-09-12 11:24:32 -0300

    Create Carta.java

---
+++ LP2/Baralho/Carta.java
@@ -0,0 +1,27 @@
+
+public class Carta {
+	
+	private String nome;
+	private String naipe;
+	
+	public Carta(String nome, String naipe) {
+		this.nome = nome;
+		this.naipe = naipe;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNaipe() {
+		return this.naipe;
+	}
+	
+	public void setNaipe(String naipe) {
+		this.naipe = naipe;
+	}
+}
