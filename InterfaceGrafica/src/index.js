56d3e9ccd34a9949da65f55ceb757de1ce665d78
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-08-18 19:08:32 -0300

    Create index.js

---
+++ InterfaceGrafica/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/Header'
+import Principal from './components/Principal';
+
+
+ReactDOM.render(
+  <>
+    <Header />
+    <Principal />
+  </>,
+  document.getElementById('root')
+);
+
