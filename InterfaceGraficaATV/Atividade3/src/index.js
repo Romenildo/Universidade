b5241fb468efd14cb426f87191fc984b887858af
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-09-06 19:04:17 -0300

    Create index.js

---
+++ InterfaceGraficaATV/Atividade3/src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import App from "./atividade3/App";
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
