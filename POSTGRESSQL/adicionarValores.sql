f81ba58294ebd43e37dfe582c972a062f9d45339
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-09-14 20:17:53 -0300

    Update adicionarValores.sql

--- POSTGRESSQL/adicionarValores.sql
+++ POSTGRESSQL/adicionarValores.sql
@@ -10,23 +10,23 @@ VALUES
 ('basilica','igreja para louvar ao senhor','1989/07/02'),
 ('catedral','igreja aberta aos domingos', '1900/02/01');
 
-INSERT INTO end_igreja(rua,bairro,numero)
+INSERT INTO end_igreja(cod_igreja,rua,bairro,numero)
 VALUES
-('centro','centro',50),
-('alpido','centro', 20),
-('olimpo','centro',192);
+(2000,'centro','centro',50),
+(2001,'alpido','centro', 20),
+(2002,'olimpo','centro',192);
 
-INSERT INTO Museu(dara_fundacao, num_salas, desc_museu)
+INSERT INTO Museu(data_fundacao, num_salas, desc_museu)
 VALUES
 ('2019/01/12',30,'museu de artes'),
 ('1976/05/03',70,'museu de historia nacional'),
-('2005/05/02', 'museu de artigos historicos da arte nacional');
+('2005/05/02',20,'museu de artigos historicos da arte nacional');
 
-INSERT INTO end_museu(rua,bairro,numero)
+INSERT INTO end_museu(cod_museu,rua,bairro,numero)
 VALUES
-('centro','centro',502),
-('alpido','centro', 230),
-('olimpo','centro',12);
+(3000,'centro','centro',502),
+(3001,'alpido','centro', 230),
+(3002,'olimpo','centro',12);
 
 INSERT INTO Fundador(nacionalidade, nome, data_nasc,data_morte,atv_profissional)
 VALUES
@@ -40,23 +40,23 @@ VALUES
 ('canto dos peixes', '2 estrelas', 'peixes brasileiros',30.00),
 ('Don quixote','5 estrelas', 'gastronimico', 200.00);
 
-INSERT INTO end_rest(rua,bairro,numero)
+INSERT INTO end_rest(cod_rest,rua,bairro,numero)
 VALUES
-('centro','centro',123),
-('alpido','centro', 220),
-('olimpo','centro',1532);
+(5000,'centro','centro',123),
+(5001,'alpido','centro', 220),
+(5002,'olimpo','centro',1532);
 
-INSERT INTO Casa_de_show(hora_abertura,dia_fechamento,descricao)
+INSERT INTO Casa_de_show(hora_abertura,dia_fechamente,descricao)
 VALUES
 ('00:19:00','quartas e quintas','casa de show punk'),
 ('00:17:00','sabados','casa de show de rock'),
 ('00:20:00','quartas e quintas','casa de show eletronica');
 
-INSERT INTO end_Casa_de_show(rua,bairro,numero)
+INSERT INTO end_show(cod_show,rua,bairro,numero)
 VALUES
-('centro','centro',123),
-('alpido','centro', 220),
-('olimpo','centro',1532);
+(4000,'centro','centro',123),
+(4001,'alpido','centro', 220),
+(4002,'olimpo','centro',1532);
 
 INSERT INTO Hotel(nome, categoria)
 VALUES
@@ -66,12 +66,12 @@ VALUES
 
 INSERT INTO Quarto(num_quarto, valor_diaria, tipo_quarto)
 VALUES
-(12,25.05,"casal"),
-(1,12.00,"Solteiro"),
-(21, 50.00,"luxo");
+(12,25.05,'casal'),
+(1,12.00,'solteiro'),
+(21, 50.00,'suit');
 
-INSERT INTO end_hotel(rua,bairro,numero)
+INSERT INTO end_hotel(cod_hotel,rua,bairro,numero)
 VALUES
-('centro','centro',1232),
-('alpido','centro', 2220),
-('olimpo','centro',32);
+(6000,'centro','centro',1232),
+(6001,'alpido','centro', 2220),
+(6002,'olimpo','centro',32);
